1c68f9d451d8db68f71db8cfc4b23fe6e372284d
ragingrahul <raj17sarma@gmail.com>
2023-06-11 19:43:53 +0530

    main page2

--- app/page.tsx
+++ app/page.tsx
@@ -12,16 +12,16 @@ const runtimeConnector:RuntimeConnector=new RuntimeConnector(Extension)
 
 export default function Home() {
 
-  const [wallet,setWallet]=useState<WALLET>()
-  const connectWallet=async()=>{
-    try {
-      const res=await runtimeConnector.connectWallet()
-      setWallet(res.wallet)
-      return res.address
-    } catch (error) {
-      console.error(error)
-    }
-  }
+  // const [wallet,setWallet]=useState<WALLET>()
+  // const connectWallet=async()=>{
+  //   try {
+  //     const res=await runtimeConnector.connectWallet()
+  //     setWallet(res.wallet)
+  //     return res.address
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
